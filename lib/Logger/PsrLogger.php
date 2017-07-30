4eb6a7f12ceeb0be6d5337043665f4aed5de3d9a
dantleech <dan.t.leech@gmail.com>
2017-07-30 12:16:06 +0200

    Added PSR logger

---
+++ lib/Logger/PsrLogger.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Phpactor\WorseReflection\Logger;
+
+use Phpactor\WorseReflection\Logger;
+use Psr\Log\LoggerInterface;
+
+final class PsrLogger implements Logger
+{ 
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public function warning(string $message)
+    {
+        $this->logger->warning($message);
+    }
+}
