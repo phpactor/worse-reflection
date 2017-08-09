59782097196c959c0fb3b3a629f4b0dce2be28cf
dantleech <dan.t.leech@gmail.com>
2017-08-09 13:40:37 +0200

    Added class-to-file source locator

---
+++ lib/Bridge/Phpactor/ClassToFileSourceLocator.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Phpactor\WorseReflection\Bridge\Phpactor;
+
+use Phpactor\ClassFileConverter\Domain\ClassToFile;
+use Phpactor\WorseReflection\Core\ClassName as WorseClassName;
+use Phpactor\WorseReflection\Core\Exception\SourceNotFound;
+use Phpactor\WorseReflection\Core\SourceCodeLocator;
+use Phpactor\WorseReflection\Core\SourceCode;
+use Phpactor\ClassFileConverter\Domain\ClassName;
+
+class ClassToFileSourceLocator implements SourceCodeLocator
+{
+    /**
+     * @var ClassToFile
+     */
+    private $converter;
+
+    public function __construct(ClassToFile $converter)
+    {
+        $this->converter = $converter;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function locate(WorseClassName $className): SourceCode
+    {
+        $candidates = $this->converter->classToFileCandidates(ClassName::fromString((string) $className));
+
+        if ($candidates->noneFound()) {
+            throw new SourceNotFound(sprintf('Could not locate a candidate for "%s"', (string) $className));
+        }
+
+        foreach ($candidates as $candidate) {
+            if (file_exists((string) $candidate)) {
+                return SourceCode::fromString(file_get_contents((string) $candidate));
+            }
+        }
+
+        throw new SourceNotFound($className);
+    }
+}
+
