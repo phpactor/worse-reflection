a736c6811daca05ead9a898df93d0e07c02a1999
dantleech <dan.t.leech@gmail.com>
2017-10-14 21:40:08 +0200

    Test exception

---
+++ tests/Unit/Core/Inference/NodeReflectorTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Phpactor\WorseReflection\Tests\Unit\Core\Inference;
+
+use PHPUnit\Framework\TestCase;
+use Phpactor\WorseReflection\Core\ServiceLocator;
+use Phpactor\WorseReflection\Core\Inference\NodeReflector;
+use Phpactor\WorseReflection\Core\Inference\Frame;
+use Microsoft\PhpParser\Node\SourceFileNode;
+
+class NodeReflectorTest extends TestCase
+{
+    public function testUnkown()
+    {
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage('Did not know how');
+        $frame = new Frame();
+        $locator = $this->prophesize(ServiceLocator::class);
+        $nodeReflector = new NodeReflector($locator->reveal());
+
+        $nodeReflector->reflectNode($frame, new SourceFileNode());
+    }
+}
