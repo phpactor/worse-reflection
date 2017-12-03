4805b6b6765ec7683d2c8b5293c4d2ef273f14cc
dantleech <dan.t.leech@gmail.com>
2017-12-03 21:10:32 +0100

    Removed untested code

--- lib/Core/Reflection/TypeResolver/PropertyTypeResolver.php
+++ lib/Core/Reflection/TypeResolver/PropertyTypeResolver.php
@@ -33,39 +33,11 @@ class PropertyTypeResolver
             return $this->property->scope()->resolveFullyQualifiedName($type, $this->property->class());
         }, $docblockTypes);
 
-        $resolvedTypes = Types::fromInferredTypes($resolvedTypes);
-
-        if (false === $this->property->docblock()->inherits()) {
-            return $resolvedTypes;
-        }
-
-        if ($this->isOverriding()) {
-            $parentProperty = $this->property->class()->parent()->propertys()->get($this->property->name());
-
-            return $resolvedTypes->merge($parentProperty->inferredReturnTypes());
-        }
-
-        $this->logger->warning(sprintf(
-            'inheritdoc used on class "%s", but class has no parent',
-            $this->property->class()->name()->full()
-        ));
-
-        return $resolvedTypes;
+        return Types::fromInferredTypes($resolvedTypes);
     }
 
     private function getDocblockTypes()
     {
         return $this->property->docblock()->varTypes();
-    }
-
-    private function isOverriding()
-    {
-        if (false === $this->property->class()->isClass()) {
-            return false;
-        }
-
-        $parent = $this->property->class()->parent();
-
-        return $parent && $parent->properties()->has($this->property->name());
     }
 }
