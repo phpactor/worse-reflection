d52436c670008db0b37f3e580b2950beb9161492
Daniel Leech <dan.t.leech@gmail.com>
2018-08-18 12:52:32 +0100

    Adds virtual reflection method

---
+++ lib/Core/Virtual/VirtualReflectionMember.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace Phpactor\WorseReflection\Core\Virtual;
+
+use Phpactor\WorseReflection\Core\DocBlock\DocBlock;
+use Phpactor\WorseReflection\Core\Inference\Frame;
+use Phpactor\WorseReflection\Core\Position;
+use Phpactor\WorseReflection\Core\Reflection\ReflectionClassLike;
+use Phpactor\WorseReflection\Core\Reflection\ReflectionMember;
+use Phpactor\WorseReflection\Core\Reflection\ReflectionScope;
+use Phpactor\WorseReflection\Core\Type;
+use Phpactor\WorseReflection\Core\Types;
+use Phpactor\WorseReflection\Core\Visibility;
+
+abstract class VirtualReflectionMember implements ReflectionMember
+{
+    /**
+     * @var Position
+     */
+    private $position;
+
+    /**
+     * @var ReflectionClassLike
+     */
+    private $declaringClass;
+
+    /**
+     * @var ReflectionClassLike
+     */
+    private $class;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var Frame
+     */
+    private $frame;
+
+    /**
+     * @var DocBlock
+     */
+    private $docblock;
+
+    /**
+     * @var ReflectionScope
+     */
+    private $scope;
+
+    /**
+     * @var Visibility
+     */
+    private $visibility;
+
+    /**
+     * @var Types
+     */
+    private $inferredTypes;
+
+    /**
+     * @var Type
+     */
+    private $type;
+
+    public function __construct(
+        Position $position,
+        ReflectionClassLike $declaringClass,
+        ReflectionClassLike $class,
+        string $name,
+        Frame $frame,
+        DocBlock $docblock,
+        ReflectionScope $scope,
+        Visibility $visiblity,
+        Types $inferredTypes,
+        Type $type
+    ) {
+        $this->position = $position;
+        $this->declaringClass = $declaringClass;
+        $this->class = $class;
+        $this->name = $name;
+        $this->frame = $frame;
+        $this->docblock = $docblock;
+        $this->scope = $scope;
+        $this->visibility = $visiblity;
+        $this->inferredTypes = $inferredTypes;
+        $this->type = $type;
+    }
+
+    public function position(): Position
+    {
+        return $this->position;
+    }
+
+    public function declaringClass(): ReflectionClassLike
+    {
+        return $this->declaringClass;
+    }
+
+    public function class(): ReflectionClassLike
+    {
+        return $this->class;
+    }
+
+    public function name(): string
+    {
+        return $this->name;
+    }
+
+    public function frame(): Frame
+    {
+        return $this->frame;
+    }
+
+    public function docblock(): DocBlock
+    {
+        return $this->docblock;
+    }
+
+    public function scope(): ReflectionScope
+    {
+        return $this->scope;
+    }
+
+    public function visibility(): Visibility
+    {
+        return $this->visibility;
+    }
+
+    public function inferredTypes(): Types
+    {
+        return $this->inferredTypes;
+    }
+
+    public function type(): Type
+    {
+        return $this->type;
+    }
+}
---
+++ tests/Unit/Core/Virtual/VirtualReflectionMethodTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Phpactor\WorseReflection\Tests\Unit\Core\Virtual;
+
+use Phpactor\WorseReflection\Core\NodeText;
+use Phpactor\WorseReflection\Core\Reflection\Collection\ReflectionParameterCollection;
+use Phpactor\WorseReflection\Core\Reflection\ReflectionMember;
+use Phpactor\WorseReflection\Core\Reflection\ReflectionMethod;
+use Phpactor\WorseReflection\Core\Virtual\VirtualReflectionMethod;
+
+class VirtualReflectionMethodTest extends VirtualReflectionMemberTestCase
+{
+    private $parameters;
+    private $body;
+    private $isAbstract;
+    private $isStatic;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->parameters = $this->prophesize(ReflectionParameterCollection::class);
+        $this->body = NodeText::fromString('hello');
+        $this->isAbstract = true;
+        $this->isStatic = true;
+    }
+
+    /**
+     * @return ReflectionMethod
+     */
+    public function member(): ReflectionMember
+    {
+        return new VirtualReflectionMethod(
+            $this->position,
+            $this->declaringClass->reveal(),
+            $this->class->reveal(),
+            $this->name,
+            $this->frame->reveal(),
+            $this->docblock->reveal(),
+            $this->scope->reveal(),
+            $this->visibility,
+            $this->types,
+            $this->type,
+            $this->parameters->reveal(),
+            $this->body,
+            $this->isAbstract,
+            $this->isStatic
+        );
+    }
+
+    public function testParameters()
+    {
+        $this->assertEquals($this->parameters->reveal(), $this->member()->parameters());
+    }
+
+    public function testBody()
+    {
+        $this->assertEquals($this->body, $this->member()->body());
+    }
+
+    public function testIsAbstract()
+    {
+        $this->assertEquals($this->isAbstract, $this->member()->isAbstract());
+    }
+
+    public function testIsStatic()
+    {
+        $this->assertEquals($this->isStatic, $this->member()->isStatic());
+    }
+}
