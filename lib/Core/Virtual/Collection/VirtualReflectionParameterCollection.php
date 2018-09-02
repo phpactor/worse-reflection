d309932178266a1be7c68bbc2cb3b75b89347f15
Daniel Leech <dan.t.leech@gmail.com>
2018-09-02 17:34:17 +0100

    Parmeter collection

---
+++ lib/Core/Virtual/Collection/VirtualReflectionParameterCollection.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Phpactor\WorseReflection\Core\Virtual\Collection;
+
+use Phpactor\WorseReflection\Core\Reflection\Collection\ReflectionParameterCollection;
+use Phpactor\WorseReflection\Core\Reflection\ReflectionParameter;
+
+class VirtualReflectionParameterCollection extends AbstractReflectionCollection implements ReflectionParameterCollection
+{
+    protected function collectionType(): string
+    {
+        return ReflectionParameterCollection::class;
+    }
+
+    public function fromReflectionParameters(array $reflectionParameters)
+    {
+        $parameters = [];
+        foreach ($reflectionParameters as $reflectionParameter) {
+            $parameters[$reflectionParameter->name()] = $reflectionParameter;
+        }
+
+        return new static($parameters);
+    }
+}
