5c49f2fd6df96e995aba72cee2c94eb6b30845eb
Daniel Leech <dan.t.leech@gmail.com>
2018-09-02 17:27:14 +0100

    Added virtual parameter

---
+++ lib/Core/Virtual/VirtualReflectionParameter.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace Phpactor\WorseReflection\Core\Virtual;
+
+use Phpactor\WorseReflection\Core\DefaultValue;
+use Phpactor\WorseReflection\Core\Position;
+use Phpactor\WorseReflection\Core\Reflection\ReflectionFunctionLike;
+use Phpactor\WorseReflection\Core\Reflection\ReflectionParameter;
+use Phpactor\WorseReflection\Core\Reflection\ReflectionScope;
+use Phpactor\WorseReflection\Core\Type;
+use Phpactor\WorseReflection\Core\Types;
+
+class VirtualReflectionParameter implements ReflectionParameter
+{
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var ReflectionFunctionLike
+     */
+    private $functionLike;
+
+    /**
+     * @var Types
+     */
+    private $inferredTypes;
+
+    /**
+     * @var Type
+     */
+    private $type;
+
+    /**
+     * @var DefaultValue
+     */
+    private $default;
+
+    /**
+     *
+     * @var bool
+     */
+    private $byReference;
+
+    /**
+     * @var ReflectionScope
+     */
+    private $scope;
+
+    /**
+     * @var Position
+     */
+    private $position;
+
+    public function __construct(
+        string $name,
+        ReflectionFunctionLike $functionLike,
+        Types $inferredTypes,
+        Type $type,
+        DefaultValue $default,
+        bool $byReference,
+        ReflectionScope $scope,
+        Position $position
+    )
+    {
+        $this->name = $name;
+        $this->functionLike = $functionLike;
+        $this->inferredTypes = $inferredTypes;
+        $this->type = $type;
+        $this->default = $default;
+        $this->byReference = $byReference;
+        $this->scope = $scope;
+        $this->position = $position;
+    }
+
+
+    public function scope(): ReflectionScope
+    {
+        return $this->scope;
+    }
+
+    public function position(): Position
+    {
+        return $this->position;
+    }
+
+    public function name(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function method(): ReflectionFunctionLike
+    {
+        return $this->functionLike;
+    }
+
+    public function functionLike(): ReflectionFunctionLike
+    {
+        return $this->functionLike;
+    }
+
+    public function type(): Type
+    {
+        return $this->type;
+    }
+
+    public function inferredTypes(): Types
+    {
+        return $this->inferredTypes;
+    }
+
+    public function default(): DefaultValue
+    {
+        return $this->default;
+    }
+
+    public function byReference(): bool
+    {
+        return $this->byReference;
+    }
+}
---
+++ tests/Unit/Core/Virtual/VirtualReflectionParameterTest.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Phpactor\WorseReflection\Tests\Unit\Core\Virtual;
+
+use PHPUnit\Framework\TestCase;
+use Phpactor\WorseReflection\Core\DefaultValue;
+use Phpactor\WorseReflection\Core\Inference\Frame;
+use Phpactor\WorseReflection\Core\NodeText;
+use Phpactor\WorseReflection\Core\Position;
+use Phpactor\WorseReflection\Core\Reflection\Collection\ReflectionParameterCollection;
+use Phpactor\WorseReflection\Core\Reflection\ReflectionClass;
+use Phpactor\WorseReflection\Core\Reflection\ReflectionMember;
+use Phpactor\WorseReflection\Core\Reflection\ReflectionMethod;
+use Phpactor\WorseReflection\Core\Reflection\ReflectionParameter;
+use Phpactor\WorseReflection\Core\Reflection\ReflectionScope;
+use Phpactor\WorseReflection\Core\Type;
+use Phpactor\WorseReflection\Core\Types;
+use Phpactor\WorseReflection\Core\Virtual\VirtualReflectionMethod;
+use Phpactor\WorseReflection\Core\Virtual\VirtualReflectionParameter;
+
+class VirtualReflectionParameterTest extends TestCase
+{
+    private $position;
+    /**
+     * @var ObjectProphecy
+     */
+    private $class;
+    /**
+     * @var string
+     */
+    private $name;
+    /**
+     * @var ObjectProphecy
+     */
+    private $frame;
+    /**
+     * @var ObjectProphecy
+     */
+    private $scope;
+    private $types;
+    /**
+     * @var Type
+     */
+    private $type;
+    /**
+     * @var ObjectProphecy
+     */
+    private $method;
+    /**
+     * @var DefaultValue
+     */
+    private $defaults;
+
+    public function setUp()
+    {
+        $this->position = Position::fromStartAndEnd(0, 0);
+        $this->class = $this->prophesize(ReflectionClass::class);
+        $this->name = 'test_name';
+        $this->scope = $this->prophesize(ReflectionScope::class);
+        $this->types = Types::empty();
+        $this->type = Type::unknown();
+        $this->method = $this->prophesize(ReflectionMethod::class);
+        $this->defaults = DefaultValue::fromValue(1234);
+        $this->byReference = false;
+    }
+
+    public function parameter(): ReflectionParameter
+    {
+        return new VirtualReflectionParameter(
+            $this->name,
+            $this->method->reveal(),
+            $this->types,
+            $this->type,
+            $this->defaults,
+            $this->byReference,
+            $this->scope->reveal(),
+            $this->position
+        );
+    }
+
+    public function testAccess()
+    {
+        $parameter = $this->parameter();
+        $this->assertEquals($this->name, $parameter->name());
+        $this->assertEquals($this->method->reveal(), $parameter->functionLike());
+        $this->assertEquals($this->method->reveal(), $parameter->method());
+        $this->assertEquals($this->types, $parameter->inferredTypes());
+        $this->assertEquals($this->type, $parameter->type());
+        $this->assertEquals($this->defaults, $parameter->default());
+        $this->assertEquals($this->byReference, $parameter->byReference());
+        $this->assertEquals($this->scope->reveal(), $parameter->scope());
+        $this->assertEquals($this->position, $parameter->position());
+    }
+}
