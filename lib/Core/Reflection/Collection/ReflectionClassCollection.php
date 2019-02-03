603201e06156a195de41505e16c519ca12664ea2
Daniel Leech <dan.t.leech@gmail.com>
2019-02-03 12:13:09 +0000

    adds docblock parameter name

--- lib/Core/Reflection/Collection/ReflectionClassCollection.php
+++ lib/Core/Reflection/Collection/ReflectionClassCollection.php
@@ -5,7 +5,7 @@ namespace Phpactor\WorseReflection\Core\Reflection\Collection;
 /**
  * @method \Phpactor\WorseReflection\Core\Reflection\ReflectionClass first()
  * @method \Phpactor\WorseReflection\Core\Reflection\ReflectionClass last()
- * @method \Phpactor\WorseReflection\Core\Reflection\ReflectionClass get()
+ * @method \Phpactor\WorseReflection\Core\Reflection\ReflectionClass get(string $name)
  */
 interface ReflectionClassCollection extends ReflectionCollection
 {
