8d1240d9bb2ebded2ce0a6c33d46fa19af46d773
dantleech <dan.t.leech@gmail.com>
2020-11-22 13:18:39 +0100

    CI phpbench (#75)
    Implements performance regression test

---
+++ .ci/phpbench-regression-test.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -e
+
+git reset --hard
+git remote add upstream https://github.com/phpactor/worse-reflection
+git fetch upstream master
+
+echo -e "\n\n"
+echo -e "Benchmarking master branch"
+echo -e "==========================\n\n"
+git checkout upstream/master
+mv composer.lock composer.lock.pr
+composer install --quiet
+vendor/bin/phpbench run --progress=none --tag=master --retry-threshold=2 --tag=master
+
+echo -e "\n\n"
+echo -e "Benchmarking current branch and comparing to master"
+echo -e "===================================================\n\n"
+git checkout -
+mv composer.lock.pr composer.lock
+composer install --quiet
+vendor/bin/phpbench run --report=aggregate_compact --progress=travis --retry-threshold=2 --uuid=tag:master 
--- tests/Benchmarks/PhpUnitReflectClassBench.php
+++ tests/Benchmarks/PhpUnitReflectClassBench.php
@@ -4,20 +4,18 @@ namespace Phpactor\WorseReflection\Tests\Benchmarks;
 
 use PHPUnit\Framework\TestCase;
 use Phpactor\WorseReflection\Core\ClassName;
-use Phpactor\WorseReflection\Bridge\TolerantParser\Reflection\ReflectionMethod;
 
 /**
- * @Iterations(4)
+ * @Iterations(5)
  * @Revs(10)
  * @Warmup(1)
  * @OutputTimeUnit("milliseconds", precision=2)
- * @Assert("variant.mode <= baseline.mode +/- 5%")
  */
 class PhpUnitReflectClassBench extends BaseBenchCase
 {
     /**
      * @Subject()
-     * @OutputTimeUnit("microseconds", precision=2)
+     * @OutputTimeUnit("microseconds", precision=4)
      */
     public function test_case(): void
     {
@@ -27,7 +25,7 @@ class PhpUnitReflectClassBench extends BaseBenchCase
     /**
      * @Subject()
      * @OutputTimeUnit("seconds", precision=2)
-     * @OutputMode("throughput", precision=2)
+     * @Assert("variant.mode <= baseline.mode +/- 5%")
      */
     public function test_case_methods_and_properties(): void
     {
@@ -48,13 +46,12 @@ class PhpUnitReflectClassBench extends BaseBenchCase
      * @Subject()
      * @Revs(1)
      * @OutputTimeUnit("seconds", precision=2)
-     * @OutputMode("throughput", precision=2)
+     * @Assert("variant.mode <= baseline.mode +/- 5%")
      */
     public function test_case_method_frames(): void
     {
         $class = $this->getReflector()->reflectClassLike(ClassName::fromString(TestCase::class));
 
-        /** @var $method ReflectionMethod */
         foreach ($class->methods() as $method) {
             $method->frame();
         }
