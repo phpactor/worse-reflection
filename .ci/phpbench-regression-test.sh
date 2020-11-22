fde79570b677a9d6a62104215bd0f95f8360ce43
Daniel Leech <dan.t.leech@gmail.com>
2020-11-22 10:54:17 +0000

    Do not re-use composer lock

--- .ci/phpbench-regression-test.sh
+++ .ci/phpbench-regression-test.sh
@@ -4,10 +4,16 @@ set -e
 git remote add upstream https://github.com/phpactor/worse-reflection
 git fetch upstream
 
+echo "Benchmarking master branch"
+echo "==========================\n\n"
 git checkout upstream/master
+mv composer.lock composer.lock.pr
 composer install
 vendor/bin/phpbench run --report=aggregate --progress=travis --tag=master --retry-threshold=2 --tag=master --progress=none
 
+echo "Benchmarking current branch and comparing to master"
+echo "===================================================\n\n"
 git checkout -
+mv composer.lock.pr composer.lock
 composer install
 vendor/bin/phpbench run --report=aggregate --progress=travis --retry-threshold=2 --uuid=tag:master --progress=none
