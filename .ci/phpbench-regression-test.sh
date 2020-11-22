9953a856d5b957ca55fc2e6c4f6325b9c32fbc57
Daniel Leech <dan.t.leech@gmail.com>
2020-11-22 12:08:16 +0000

    Use printf instead of echo

--- .ci/phpbench-regression-test.sh
+++ .ci/phpbench-regression-test.sh
@@ -5,17 +5,17 @@ git reset --hard
 git remote add upstream https://github.com/phpactor/worse-reflection
 git fetch upstream master
 
-echo "\n\n"
-echo "Benchmarking master branch"
-echo "==========================\n\n"
+printf "\n\n"
+printf "Benchmarking master branch"
+printf "==========================\n\n"
 git checkout upstream/master
 mv composer.lock composer.lock.pr
 composer install --quiet
 vendor/bin/phpbench run --progress=none --tag=master --retry-threshold=2 --tag=master
 
-echo "\n\n"
-echo "Benchmarking current branch and comparing to master"
-echo "===================================================\n\n"
+printf "\n\n"
+printf "Benchmarking current branch and comparing to master"
+printf "===================================================\n\n"
 git checkout -
 mv composer.lock.pr composer.lock
 composer install --quiet
