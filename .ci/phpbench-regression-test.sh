0677c564fadb4e8995776eb87924086b5a52d8ab
Daniel Leech <dan.t.leech@gmail.com>
2020-11-22 12:09:13 +0000

    Echo -e

--- .ci/phpbench-regression-test.sh
+++ .ci/phpbench-regression-test.sh
@@ -5,17 +5,17 @@ git reset --hard
 git remote add upstream https://github.com/phpactor/worse-reflection
 git fetch upstream master
 
-printf "\n\n"
-printf "Benchmarking master branch"
-printf "==========================\n\n"
+echo -e "\n\n"
+echo -e "Benchmarking master branch"
+echo -e "==========================\n\n"
 git checkout upstream/master
 mv composer.lock composer.lock.pr
 composer install --quiet
 vendor/bin/phpbench run --progress=none --tag=master --retry-threshold=2 --tag=master
 
-printf "\n\n"
-printf "Benchmarking current branch and comparing to master"
-printf "===================================================\n\n"
+echo -e "\n\n"
+echo -e "Benchmarking current branch and comparing to master"
+echo -e "===================================================\n\n"
 git checkout -
 mv composer.lock.pr composer.lock
 composer install --quiet
