fe8564f82319674487fc313ab10b5f53e540c06f
Daniel Leech <dan.t.leech@gmail.com>
2020-11-22 10:42:14 +0000

    Upda

--- .ci/phpbench-regression-test.sh
+++ .ci/phpbench-regression-test.sh
@@ -3,10 +3,11 @@ set -e
 
 git remote add upstream https://github.com/phpactor/worse-reflection
 git fetch upstream
+
 git checkout upstream/master
 composer install
-vendor/bin/phpbench run --report=aggregate --progress=travis --tag=master --retry-threshold=2 --tag=master --progress=dots
+vendor/bin/phpbench run --report=aggregate --progress=travis --tag=master --retry-threshold=2 --tag=master --progres=none
 
 git checkout -
 composer install
-vendor/bin/phpbench run --report=aggregate --progress=travis --retry-threshold=2 --uuid=tag:master --progress=dots
+vendor/bin/phpbench run --report=aggregate --progress=travis --retry-threshold=2 --uuid=tag:master --progress=none
