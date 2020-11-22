6c8b55060a94cda190ed4e5c02da6179de234139
Daniel Leech <dan.t.leech@gmail.com>
2020-11-22 17:12:18 +0000

    Allow retry threshold from env

--- .ci/phpbench-regression-test.sh
+++ .ci/phpbench-regression-test.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
+RETRY_THRESHOLD=${RETRY_THRESHOLD:-5}
+
 git reset --hard
-git remote add upstream https://github.com/phpactor/worse-reflection
+git remote add upstream https://github.com/phpactor/worse-reflection || yes
 git fetch upstream master
 
 echo -e "\n\n"
@@ -11,7 +13,7 @@ echo -e "==========================\n\n"
 git checkout upstream/master
 mv composer.lock composer.lock.pr
 composer install --quiet
-vendor/bin/phpbench run --report=aggregate_compact --progress=dots --retry-threshold=2 --tag=master
+vendor/bin/phpbench run --report=aggregate_compact --progress=dots --retry-threshold=$RETRY_THRESHOLD --tag=master
 
 echo -e "\n\n"
 echo -e "Benchmarking current branch and comparing to master"
@@ -19,4 +21,4 @@ echo -e "===================================================\n\n"
 git checkout -
 mv composer.lock.pr composer.lock
 composer install --quiet
-vendor/bin/phpbench run --report=aggregate_compact --progress=dots --retry-threshold=2 --uuid=tag:master 
+vendor/bin/phpbench run --report=aggregate_compact --progress=dots --retry-threshold=$RETRY_THRESHOLD --uuid=tag:master 
