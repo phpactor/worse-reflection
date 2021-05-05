ba68c3e8fe598b73ded2c6d6cea6d4899b10c6ae
Daniel Leech <dan.t.leech@gmail.com>
2021-05-05 19:47:34 +0100

    Fix regression test

--- .github/phpbench_regression.sh
+++ .github/phpbench_regression.sh
@@ -10,7 +10,7 @@ git fetch origin master
 git checkout master
 mv composer.lock composer.lock.pr
 composer install --quiet
-vendor/bin/phpbench run --report=aggregate_compact --progress=travis --retry-threshold=$RETRY_THRESHOLD --tag=master
+vendor/bin/phpbench run --report=aggregate_compact --progress=plain --retry-threshold=$RETRY_THRESHOLD --tag=master
 
 echo -e "\n\n"
 echo -e "Benchmarking GITHUB_REF and comparing to master"
@@ -19,4 +19,4 @@ git checkout -
 git status
 mv composer.lock.pr composer.lock
 composer install --quiet
-vendor/bin/phpbench run --report=aggregate_compact --progress=travis --retry-threshold=$RETRY_THRESHOLD --uuid=tag:master 
+vendor/bin/phpbench run --report=aggregate_compact --progress=plain --retry-threshold=$RETRY_THRESHOLD --uuid=tag:master 
